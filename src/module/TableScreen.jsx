999ca4edc6b3f45dc74d2327b0a2ef574044e6e5
Miroslav Grund <miroslavgrund68@gmail.com>
2026-04-26 15:42:21 +0200

    Created table

--- src/module/TableScreen.jsx
+++ src/module/TableScreen.jsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import { Container } from 'reactstrap';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { Container } from "reactstrap";
+import { useTranslation } from "react-i18next";
+import { DataTableCard2 } from "asab_webui_components";
 
 export function TableScreen(props) {
-	const { t } = useTranslation();
+  const { t } = useTranslation();
 
-	return (
-		<Container className='h-100'>
-			{t('Training|Hello, there is nothing here yet!')}
-		</Container>
-	);
+  const tableLoader = async ({ params, setRows, setCount }) => {
+    try {
+      const response = await fetch("https://devtest.teskalabs.com/data");
+
+      if (!response.ok) {
+        throw new Error(`HTTP chyba! Status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log("Data: ", result);
+
+      return {
+        count: result.total,
+        rows: result.data,
+      };
+    } catch (error) {
+      console.error(error);
+      return { count: 0, rows: [] };
+    }
+  };
+
+  const columns = [
+    { title: "ID", render: ({ row }) => row.id },
+    { title: "Username", render: ({ row }) => row.username },
+    { title: "Email", render: ({ row }) => row.email },
+    { title: "Address", render: ({ row }) => row.address },
+    { title: "Created", render: ({ row }) => row.created },
+    { title: "Last Sign In", render: ({ row }) => row.last_sign_in },
+  ];
+
+  return (
+    <Container className="h-100">
+      <DataTableCard2
+        columns={columns}
+        loader={tableLoader}
+        header={<div>{t("Training|Users")}</div>}
+      />
+    </Container>
+  );
 }
